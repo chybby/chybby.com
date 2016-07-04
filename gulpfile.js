caee1b54f7dabaf8aa0610fcebefb3abd235fd9e
Chybby <achybby@gmail.com>
2016-07-05 04:55:34 +1000

    started using gulp and converted css to sass

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var notify = require('gulp-notify');
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('css');
+});
+
+gulp.task('clean', function() {
+    return del(['static/css']);
+});
+
+gulp.task('css', function() {
+  return sass('css/chybby.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('static/css'))
+    .pipe(notify({ message: 'CSS task complete' }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('css/*.scss', ['css']);
+});
