bb6ee3bf73f3c0bce8f811d8bb6219d097ae31a0
chybby <achybby@gmail.com>
2017-09-02 01:19:41 +0200

    Added docstrings

--- chybby.py
+++ chybby.py
@@ -1,33 +1,31 @@
-from __future__ import (
-    absolute_import,
-    print_function,
-)
+'''Adam Chyb's personal website.'''
 
-import json
+from __future__ import absolute_import, print_function
 
-from flask import (
-    Flask,
-    g,
-    render_template,
-    send_from_directory,
-)
+import os
+
 import flask_assetrev
+from flask import Flask, g, render_template, send_from_directory
 
 app = Flask(__name__)
 app.config.from_object('config')
 flask_assetrev.AssetRev(app)
 
-def after_this_request(f):
+
+def after_this_request(func):
+    '''Adds the given function to the current request's callbacks.'''
     if not hasattr(g, 'after_request_callbacks'):
         g.after_request_callbacks = []
-    g.after_request_callbacks.append(f)
-    return f
+    g.after_request_callbacks.append(func)
+    return func
 
 
 @app.route('/', methods=['GET'])
 def homepage():
+    '''Render the homepage.'''
     @after_this_request
-    def add_header(response):
+    def add_no_cache_header(response):
+        '''Adds a no cache header to the response.'''
         response.cache_control.no_store = True
         return response
 
@@ -36,12 +34,14 @@ def homepage():
 
 @app.route('/favicon.ico')
 def favicon():
+    '''Delivers the favicon'''
     return send_from_directory(os.path.join(app.root_path, 'static/favicon'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.after_request
 def call_after_request_callbacks(response):
+    '''Calls the after request callbacks and sets a default caching policy.'''
     for callback in getattr(g, 'after_request_callbacks', ()):
         callback(response)
 
